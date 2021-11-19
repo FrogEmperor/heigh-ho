958cd98dec3d0a6f19f733e8d87db54ec26d5619
FrogEmperor <oscar_tj2000@hotmail.com>
2021-11-18 17:09:41 -0800

    LESGOO

--- main.cu
+++ main.cu
@@ -1,17 +1,19 @@
 #include <iostream>
-
 #include "blockchain.hpp"
+#include "cuda_runtime.h"
 
+__global__ void kernel(){
+
+}
 
 int main() {
-    Blockchain btc = Blockchain(3);
-
+    Blockchain btc = Blockchain(7);
     btc.newTransaction("oscar", "manolo", 1);
     btc.newTransaction("manolo", "dagz", 2);
 
     int lastProof = btc.lastBlock().proof;
     int newProof = btc.proofOfWork(lastProof);
     btc.newBlock(newProof, std::to_string(lastProof));
-
+    kernel<<<1,1>>>();
     return 0;
 }
